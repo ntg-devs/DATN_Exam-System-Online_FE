a4a5460b073b9451bc747e64f261be3ee5210904
ntg-devs <giangnguyendev99@gmail.com>
2025-12-13 08:48:09 +0700

    Version 4 - Change URL

--- src/utils/path.js
+++ src/utils/path.js
@@ -1,7 +1,7 @@
 // export const URL_API = "http://localhost:8000/api";
 // export const SOCKET_URL = "ws://localhost:8000"
-// const NGROK = "unworkable-bernie-merely.ngrok-free.dev";
-const NGROK = "halle-uranylic-sky.ngrok-free.dev";
+const NGROK = "unworkable-bernie-merely.ngrok-free.dev";
+// const NGROK = "halle-uranylic-sky.ngrok-free.dev";
 
 export const URL_API = `https://${NGROK}/api`;
 export const SOCKET_URL = `wss://${NGROK}`;
