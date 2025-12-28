c5ab855e4ee9a2c8a37d648d81271c907648ffb9
ntg-devs <giangnguyendev99@gmail.com>
2025-12-28 17:33:01 +0700

    Version 8 --change 2

--- src/utils/path.js
+++ src/utils/path.js
@@ -3,7 +3,8 @@
 // const NGROK = "unworkable-bernie-merely.ngrok-free.dev"; -> Hết hạn
 // const NGROK = "halle-uranylic-sky.ngrok-free.dev";
 // const NGROK = "yuko-unequestrian-family.ngrok-free.dev"; -> Không sài được
-const NGROK = "noncoercive-lowell-unmoderating.ngrok-free.dev";
+// const NGROK = "noncoercive-lowell-unmoderating.ngrok-free.dev";
+const NGROK = "hypereutectic-caenogenetically-latina.ngrok-free.dev";
 
 export const URL_API = `https://${NGROK}/api`;
 export const SOCKET_URL = `wss://${NGROK}`;
